c87ad9547c5abc9883cb3c7b60cc6223361c4168
claudia.tanase <claudia.tanase@c52ec8a4-9903-11dd-b35e-65caead18e0b>
2008-11-11 21:49:09 +0000

    Added ability to send messages from kernel to user application.

--- trunk/src/xerxes/soaproject/interceptor/user/intercept.c
+++ trunk/src/xerxes/soaproject/interceptor/user/intercept.c
@@ -14,46 +14,47 @@ struct msghdr msg;
 
 
 int main(void) {
- sock_fd = socket(PF_NETLINK, SOCK_RAW,NETLINK_UNUSED);
 
- memset(&src_addr, 0, sizeof(src_addr));
- src_addr.nl_family = AF_NETLINK;
- src_addr.nl_pid = getpid();  /* self pid */
- src_addr.nl_groups = 0;  /* not in mcast groups */
+	sock_fd = socket(PF_NETLINK, SOCK_RAW,NETLINK_UNUSED);
 
- bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
+	memset(&src_addr, 0, sizeof(src_addr));
+	src_addr.nl_family = AF_NETLINK;
+	src_addr.nl_pid = getpid();  /* self pid */
+	src_addr.nl_groups = 0;  /* not in mcast groups */
 
- memset(&dest_addr, 0, sizeof(dest_addr));
- dest_addr.nl_family = AF_NETLINK;
- dest_addr.nl_pid = 0;   /* For Linux Kernel */
- dest_addr.nl_groups = 0; /* unicast */
+	bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
 
- nlh=(struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+	memset(&dest_addr, 0, sizeof(dest_addr));
+	dest_addr.nl_family = AF_NETLINK;
+	dest_addr.nl_pid = 0;   /* For Linux Kernel */
+	dest_addr.nl_groups = 0; /* unicast */
 
- /* Fill the netlink message header */
- nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
- nlh->nlmsg_pid = getpid();  /* self pid */
- nlh->nlmsg_flags = 0;
+	nlh=(struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
 
- /* Fill in the netlink message payload */
- strcpy(NLMSG_DATA(nlh), "Hello you! This is Userspace App");
+	/* Fill the netlink message header */
+	nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+	nlh->nlmsg_pid = getpid();  /* self pid */
+	nlh->nlmsg_flags = 0;
 
- iov.iov_base = (void *)nlh;
- iov.iov_len = nlh->nlmsg_len;
- msg.msg_name = (void *)&dest_addr;
- msg.msg_namelen = sizeof(dest_addr);
- msg.msg_iov = &iov;
- msg.msg_iovlen = 1;
+	/* Fill in the netlink message payload */
+	strcpy(NLMSG_DATA(nlh), "Hello you! This is Userspace App");
 
- sendmsg(sock_fd, &msg, 0);
+	iov.iov_base = (void *)nlh;
+	iov.iov_len = nlh->nlmsg_len;
+	msg.msg_name = (void *)&dest_addr;
+	msg.msg_namelen = sizeof(dest_addr);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
 
- /* Read message from kernel */
- memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
- recvmsg(sock_fd, &msg, 0);
- printf(" Received message payload: %s\n", NLMSG_DATA(nlh));
+	sendmsg(sock_fd, &msg, 0);
 
- /* Close Netlink Socket */
- close(sock_fd);
+	/* Read message from kernel */
+	memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+	recvmsg(sock_fd, &msg, 0);
+	printf(" Received message payload: %s\n", NLMSG_DATA(nlh));
 
- return 0;
+	/* Close Netlink Socket */
+	close(sock_fd);
+
+	return 0;
 }
