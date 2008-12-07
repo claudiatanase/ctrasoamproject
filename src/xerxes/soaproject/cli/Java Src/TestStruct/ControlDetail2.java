ade0809968910bbe3cdb857626871bce097803ab
claudia.tanase <claudia.tanase@c52ec8a4-9903-11dd-b35e-65caead18e0b>
2008-12-07 14:24:46 +0000

    Calling Java from C works now. :D

---
+++ src/xerxes/soaproject/cli/Java Src/TestStruct/ControlDetail2.java
@@ -0,0 +1,15 @@
+public class ControlDetail2
+{
+	public int ID;
+	public String path;
+	public String tag;
+	public int cmd;	
+	
+	public ControlDetail2(int nID, String szTag, String szPath, int nCmd)
+	{
+		this.ID = nID;
+		this.path = szPath;
+		this.tag = szTag;
+		this.cmd = nCmd;
+	}
+}
